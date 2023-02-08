44cc7f99f37cb2baf1d53de79923105c8594cdb4
Maulik <varshneymaulik05@gmail.com>
2023-02-08 13:35:55 +0000

    First BitManipulationCode

---
+++ Bit Manipulation and Advance Maths/IntroductionToBitManipulation.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bitManipulation(int num, int i) {
+        int g=(num & (1<<(i-1)));
+       if(g){
+           cout<<1<<" ";
+        }
+       
+       else{
+           cout<<0<<" ";
+       }
+        int s=(num|(1<<(i-1)));
+        cout<<s<<" ";
+        int c=(num & (~(1<<(i-1))));
+        cout<<c<<" ";
+}
+ int main(){
+    int num, i;
+    cin>>num>>i;
+    bitManipulation(num,i);
+    return 0;
+ }
